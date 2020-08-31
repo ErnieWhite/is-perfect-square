37a16ffceb0dde214760aace477f4482fd887f0c
Archie  White <erniewhite@embarqmail.com>
2020-08-31 17:37:06 -0500

    Added prime_factors and prime_numbers functions

--- perfect_square.py
+++ perfect_square.py
@@ -12,6 +12,19 @@ def sieve(s: Iterator[int]) -> Generator[int, None, None]:
     yield from sieve(i for i in s if i % n != 0)
 
 
+def prime_numbers(n: int) -> list:
+    primes = [2, ]
+    for i in range(2, n):
+        if all((i % x) != 0 for x in primes):
+            primes.append(i)
+    return primes
+
+
+def prime_factors(n: int) -> tuple:
+    primes = prime_numbers(n)
+    return tuple(x for x in primes if n % x == 0)
+
+
 def is_perfect_square(n: int) -> None:
     p: Generator[int, None, None] = sieve(iter(range(2, n)))
     a: int = next(p)
@@ -21,4 +34,5 @@ def is_perfect_square(n: int) -> None:
 
 
 if __name__ == '__main__':
-    is_perfect_square(100)
+    print(prime_numbers(100))
+    print(prime_factors(100))
